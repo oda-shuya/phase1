de0d276a36bf28ecf35ac3eff0b4ffcd69bea9ef
becchi <mebiusu1968@gmail.com>
2025-02-19 01:26:27 +0900

    servletでHelloWorldを出力

---
+++ hello-servlet/src/main/java/com/example/helloservlet/HelloWorldServlet.java
@@ -0,0 +1,13 @@
+package com.example.helloservlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class HelloWorldServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        response.getWriter().println("<h1>Hello, World!</h1>");
+    }
+}
